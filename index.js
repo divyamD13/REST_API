9b1e7963c0d9ce3bff1d390406e712781cf97078
Divyam Divesh <divyamdivesh13@gmail.com>
2025-03-19 20:37:13 +0530

    added mock data

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const express = require("express");
+const users= require("./MOCK_DATA.json");
 
 const app= express();
 
-const PORT=8000;
+app.get("/api/users",(req,res)=>
+    res.json(users)
+);
 
+app.get("/users",(req,res)=>{
+const html=`<ul>
+${users.map((user)=>`<li>${user.first_name}</li>`
+).join("")}
+</ul>`;
+res.send(html);
+});
+
+app.route("/api/users/:id").get((req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find((user)=>user.id===id);
+    return  res.send(user);
+}).put((req,res)=>{
+    return res.json({status:'pending'});
+}).patch((req,res)=>{
+    return res.json({status:'pending'});
+}).delete((req,res)=>{
+    return res.json({status:'pending'});
+})
+// app.get("/api/users/:id",(req,res)=>{
+//     const id=Number(req.params.id);
+//     const user=users.find((user)=>user.id===id);
+//     return  res.send(user);
+// });
+
+const PORT=8000;
 app.listen(PORT, ()=>console.log(`Server started at PORT: ${PORT}`));
