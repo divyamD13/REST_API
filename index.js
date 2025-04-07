5f9dc67060f4ad89c2e89f68b4775f87390f86b9
Divyam Divesh <divyamdivesh13@gmail.com>
2025-04-07 15:21:12 +0530

    added status code for different errors

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const app= express();
 
 app.use(express.urlencoded({extended:false}));
 
-app.get("/api/users",(req,res)=>
-    res.json(users)
+app.get("/api/users",(req,res)=>{
+    res.setHeader("myName", "Divyam");
+    return res.json(users);
+}
+    
 );
 
 app.get("/users",(req,res)=>{
@@ -19,8 +22,12 @@ res.send(html);
 });
 
 app.route("/api/users/:id").get((req,res)=>{
+    
     const id=Number(req.params.id);
     const user=users.find((user)=>user.id===id);
+    if(!user){
+        return res.status(404).json({status:'failed', message:'User not found'});
+    }
     return  res.send(user);
 }).put((req,res)=>{
     return res.json({status:'pending'});
@@ -36,10 +43,13 @@ app.route("/api/users/:id").get((req,res)=>{
 // });
 app.post("/api/users",(req,res)=>{
     const body = req.body;
+    if(!body || !body.first_name || !body.last_name || !body.email ||!body.gender || !body.job_title ){
+        return res.status(400).json({status:'failed', message:'Please provide all the fields'});
+    }
     users.push({...body, id: users.length+1});
     fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
         // console.log("Body :", body);
-    return res.json({status:'success', id: users.length});
+    return res.status(201).json({status:'success', id: users.length});
     })
 });
 
